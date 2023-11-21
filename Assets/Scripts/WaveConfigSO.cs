6cad0c1b6ab8604e3a29b08d5e690f612414fd59
Oğulcan Özen <nm-can@hotmail.com>
2023-11-21 17:46:04 +0300

    Enemy implemented

---
+++ Assets/Scripts/WaveConfigSO.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")] 
+public class WaveConfigSO : ScriptableObject
+{
+    [SerializeField] Transform pathPrefab;
+    [SerializeField] List<GameObject> enemyPrefabs;
+    [SerializeField] float moveSpeed = 5f;
+
+    public int GetEnemyCount()
+    {
+        return enemyPrefabs.Count;
+    }
+
+    public GameObject GetEnemyPrefab(int index)
+    {
+        return enemyPrefabs[index];
+    }
+
+
+    public Transform GetStartingWaypoint()
+    {
+        return pathPrefab.GetChild(0);
+    }
+
+    public List<Transform> GetWaypoints()
+    {
+        List<Transform> waypoints = new List<Transform>();
+        foreach(Transform child in pathPrefab)
+        {
+            waypoints.Add(child);
+        }
+        return waypoints;
+    }
+
+    public float GetMoveSpeed()
+    {
+        return moveSpeed;
+    }
+}
